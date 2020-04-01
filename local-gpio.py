9272b0e8af7c5b19105455829fbed0bfa87381ce
acarlson98 <acarlson98@gmail.com>
2020-04-01 17:15:44 -0500

    modified local-gpio to work with motor

--- local-gpio.py
+++ local-gpio.py
@@ -2,46 +2,38 @@ import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
  
+DIR = 20
+STEP = 21
+CW = 1
+CCW = 0
+SPR = 48
+
 def on_connect(client, userdata, flag, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("isyjp/gpio21")
  
 def on_message(client, userdata, msg):
      print(msg.topic+" "+str(msg.payload))
-     GPIO.output(21,1)
-     GPIO.output(20,1)
-     GPIO.output(16,1)
-     GPIO.output(12,1)
+     GPIO.output(DIR,1)
+     GPIO.output(STEP,1)
 
-    # 21 = red
-    # 20 = yellow
-    # 16 = blue
-    # 12 = green
-
-     if msg.payload == "up":
-         GPIO.output(21,1)
-         GPIO.output(20,0)
-         GPIO.output(16,0)
-         GPIO.output(12,0)
-         print("up")
-     if msg.payload == "down":
-         GPIO.output(21,0)
-         GPIO.output(20,1)
-         GPIO.output(16,0)
-         GPIO.output(12,0)
-         print("down")
-     if msg.payload == "left":
-         GPIO.output(21,0)
-         GPIO.output(20,0)
-         GPIO.output(16,1)
-         GPIO.output(12,0)
-         print("left")
      if msg.payload == "right":
-         GPIO.output(21,0)
-         GPIO.output(20,0)
-         GPIO.output(16,0)
-         GPIO.output(12,1)
+         GPIO.output(DIR, CW)
+         for x in range(SPR):
+            GPIO.output(STEP, 1)
+            time.sleep(0.01)
+            GPIO.output(STEP, 0)
+            time.sleep(0.01)
          print("right")
+     if msg.payload == "left":
+         GPIO.output(DIR, CCW)
+         for x in range(SPR):
+            GPIO.output(STEP, 1)
+            time.sleep(0.01)
+            GPIO.output(STEP, 0)
+            time.sleep(0.01)
+         print("left")
+         
  
 def on_disconnect():
     pass
